fbbca6bd8c4e4ef98b038792b4be2c38b8ee3515
Alexandre Bensarsa <alexandre.bensarsa@jeece.fr>
2023-12-26 17:59:19 +0100

    fix: iac structure

--- iac/userapi/src/client.connected.js
+++
@@ -1,8 +0,0 @@
-// Assuming 'client' is a WebSocket client
-if (client.connected) {
-    // The client is connected, safe to send/receive data
-    client.send("Hello, server!");
-} else {
-    // The client is not connected
-    console.log("Connection is closed.");
-}
